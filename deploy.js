a4fbca79441e842d3f2ba2425c4272730151c4f1
Jason Stidd <j.doug.stidd@gmail.com>
2020-04-01 21:24:52 -0600

    update EC2 specific information

--- deploy.js
+++ deploy.js
@@ -85,9 +85,9 @@ function sshConnect() {
   ssh
     .connect({
       // TODO: ADD YOUR IP ADDRESS BELOW (e.g. '12.34.5.67')
-      host: '00.00.00.00',
+      host: '184.72.207.103',
       username: 'ubuntu',
-      privateKey: 'hs-key.pem'
+      privateKey: '/users/jstidd/Documents/hs-key.pem'
     })
     .then(function() {
       console.log('SSH Connection established.');
